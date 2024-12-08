316da41cc4aa7b7ac0da2a7561c2de855227622d
forbenaj <benjaminbustelo@gmail.com>
2024-12-08 15:59:13 -0300

    wip refactor

--- js/player.js
+++ js/player.js
@@ -1,4 +1,110 @@
+class Player{
+    constructor(){
+        this.conn = null
+        this.pressedButtons = []
+        this.touchMap = new Map()
+        this.myself = null
+    }
+    connectToHost(hostName){
+        this.conn = this.myself.connect(hostName);
+    
+        this.conn.on('open', () => {
+            console.log("You connected to "+this.conn.peer)
+            document.getElementById('joystickContainer').style.display = "grid"
+            this.createController()
+            mainMenu.remove()
+          });
+    
+          this.conn.on('error', (err) => {
+           console.log(err.type)
+        });
+    }
+    sendData(data){
+        this.conn.send(data)
+    }
+    createController() {
+        document.addEventListener('touchstart', (e) => {
+            for (let i = 0; i < e.touches.length; i++) {
+                let touch = e.touches[i]
+                const touchX = touch.clientX;
+                const touchY = touch.clientY;
+    
+            const element = document.elementFromPoint(touchX, touchY);
+    
+                if (element.classList.contains("controller-button")) {
+                    const buttonId = element.id;
+    
+                    if (!this.pressedButtons.includes(buttonId)) {
+                        this.pressedButtons.push(buttonId)
+    
+                        this.conn.send({action: buttonId, attr: 1})
+                        element.classList.add("pressed")
+                        this.touchMap.set(touch.identifier, buttonId)
+                    }
+                }
+            }
+        }, {passive: false});
+    
+        document.addEventListener('touchend', (e) => {
+            for (let i = 0; i < e.changedTouches.length; i++) {
+                let touch = e.changedTouches[i]
+                if (touchMap.has(touch.identifier)) {
+                    const buttonId = this.touchMap.get(touch.identifier)
+                    this.conn.send({action: buttonId, attr: 0})
+                    this.pressedButtons.splice(this.pressedButtons.indexOf(buttonId), 1)
+                    document.getElementById(buttonId).classList.remove("pressed")
+                    this.touchMap.delete(touch.identifier)
+                }
+            }
+        });
+    
+    
+        document.addEventListener('touchmove', (e) => {
+            for (let i = 0; i < e.touches.length; i++) {
+                let touch = e.touches[i]
+                const touchX = touch.clientX;
+                const touchY = touch.clientY;
+    
+                const element = document.elementFromPoint(touchX, touchY);
+    
+                if (element.classList.contains("controller-button")) {
+                    const buttonId = element.id;
+    
+                    if (touchMap.has(touch.identifier)) {
+                        if (buttonId !== this.touchMap.get(touch.identifier)) {
+                            this.conn.send({action: this.touchMap.get(touch.identifier), attr: 0})
+                            this.pressedButtons.splice(pressedButtons.indexOf(this.touchMap.get(touch.identifier)), 1)
+                            document.getElementById(this.touchMap.get(touch.identifier)).classList.remove("pressed")
+                            this.touchMap.delete(touch.identifier)
+                        }
+                    }
+                    if (!this.pressedButtons.includes(buttonId)) {
+                        this.pressedButtons.push(buttonId)
+                        this.conn.send({action: buttonId, attr: 1})
+                        element.classList.add("pressed")
+                        this.touchMap.set(touch.identifier, buttonId)
+                    }
+                }
+                else {
+                    if (this.touchMap.has(touch.identifier)) {
+                        const buttonId = this.touchMap.get(touch.identifier)
+                        this.conn.send({action: buttonId, attr: 0})
+                        this.pressedButtons.splice(this.pressedButtons.indexOf(buttonId), 1)
+                        document.getElementById(buttonId).classList.remove("pressed")
+                        this.touchMap.delete(touch.identifier)
+                    }
+                }
+            }
+        });
+    
+        toggleScreenOrientation()
+    
+        loaderContainer.style.display = "none"
+    }
+}
+
 function enterAsPlayer(){
+    let player = new Player()
     let idInput = document.getElementById("id-input").value
     if(idInput==""){
         inputErrorMessage.innerText = "Insert a host name"
@@ -6,19 +112,19 @@ function enterAsPlayer(){
     }
     loaderContainer.style.display = "flex"
     device_mode = "player"
-    myself = new Peer();
-    myself.on('open', function(id) {
+    player.myself = new Peer();
+    player.myself.on('open', function(id) {
         console.log('Connected. My peer ID is: ' + id);
-        connectToHost(idInput)
+        player.connectToHost(idInput)
       });
     
-    myself.on('connection', function(recievingConn) {
+      player.myself.on('connection', function(recievingConn) {
         console.log("New peer connected: "+recievingConn.peer)
-        conn = recievingConn
-        players.push(conn)
+        player.conn = recievingConn
+        players.push(player.conn)
     });
 
-    myself.on('error', (err) => {
+    player.myself.on('error', (err) => {
         if (err.type === 'unavailable-id') {
             console.log('The peer ID is already in use. Generating new');
             enterAsPlayer()
@@ -35,22 +141,6 @@ function enterAsPlayer(){
 }
 
 
-function connectToHost(hostName){
-    conn = myself.connect(hostName);
-
-    conn.on('open', function() {
-        console.log("You connected to "+conn.peer)
-        document.getElementById('joystickContainer').style.display = "grid"
-        createController()
-        mainMenu.remove()
-      });
-
-      conn.on('error', (err) => {
-       console.log(err.type)
-    });
-}
-
-
 function activateCounter(button) {
     button.time = 0;
 
@@ -62,91 +152,6 @@ function activateCounter(button) {
         }
     }, 1);
 }
-
-
-let pressedButtons = []
-let touchMap = new Map()
-
-function createController() {
-    document.addEventListener('touchstart', (e) => {
-        for (let i = 0; i < e.touches.length; i++) {
-            let touch = e.touches[i]
-            const touchX = touch.clientX;
-            const touchY = touch.clientY;
-
-        const element = document.elementFromPoint(touchX, touchY);
-
-            if (element.classList.contains("controller-button")) {
-                const buttonId = element.id;
-
-                if (!pressedButtons.includes(buttonId)) {
-                    pressedButtons.push(buttonId)
-
-                    conn.send({action: buttonId, attr: 1})
-                    element.classList.add("pressed")
-                    touchMap.set(touch.identifier, buttonId)
-                }
-            }
-        }
-    }, {passive: false});
-
-    document.addEventListener('touchend', (e) => {
-        for (let i = 0; i < e.changedTouches.length; i++) {
-            let touch = e.changedTouches[i]
-            if (touchMap.has(touch.identifier)) {
-                const buttonId = touchMap.get(touch.identifier)
-                conn.send({action: buttonId, attr: 0})
-                pressedButtons.splice(pressedButtons.indexOf(buttonId), 1)
-                document.getElementById(buttonId).classList.remove("pressed")
-                touchMap.delete(touch.identifier)
-            }
-        }
-    });
-
-
-    document.addEventListener('touchmove', (e) => {
-        for (let i = 0; i < e.touches.length; i++) {
-            let touch = e.touches[i]
-            const touchX = touch.clientX;
-            const touchY = touch.clientY;
-
-            const element = document.elementFromPoint(touchX, touchY);
-
-            if (element.classList.contains("controller-button")) {
-                const buttonId = element.id;
-
-                if (touchMap.has(touch.identifier)) {
-                    if (buttonId !== touchMap.get(touch.identifier)) {
-                        conn.send({action: touchMap.get(touch.identifier), attr: 0})
-                        pressedButtons.splice(pressedButtons.indexOf(touchMap.get(touch.identifier)), 1)
-                        document.getElementById(touchMap.get(touch.identifier)).classList.remove("pressed")
-                        touchMap.delete(touch.identifier)
-                    }
-                }
-                if (!pressedButtons.includes(buttonId)) {
-                    pressedButtons.push(buttonId)
-                    conn.send({action: buttonId, attr: 1})
-                    element.classList.add("pressed")
-                    touchMap.set(touch.identifier, buttonId)
-                }
-            }
-            else {
-                if (touchMap.has(touch.identifier)) {
-                    const buttonId = touchMap.get(touch.identifier)
-                    conn.send({action: buttonId, attr: 0})
-                    pressedButtons.splice(pressedButtons.indexOf(buttonId), 1)
-                    document.getElementById(buttonId).classList.remove("pressed")
-                    touchMap.delete(touch.identifier)
-                }
-            }
-        }
-    });
-
-    toggleScreenOrientation()
-
-    loaderContainer.style.display = "none"
-}
-
 
 let screenOrientation = "portrait"
 
