95398ad4319f8c08481c25c28da597208402852e
Benjamin B <benjaminb@brightsector.com>
2024-12-21 15:10:10 -0300

    added console

--- js/main.js
+++ js/main.js
@@ -4,7 +4,12 @@ let mainMenu = document.getElementById("mainMenu")
 
 let loaderContainer = document.getElementById("loader-container")
 
+let consoleDiv = document.getElementById("consoleDiv")
+
 let forceFullscreen = false
+
+const urlParams = new URLSearchParams(window.location.search);
+let showConsole = urlParams.has('console') || false
 
 let idInput = document.getElementById('id-input')
 
@@ -49,3 +54,59 @@ async function requestWakeLock() {
 }
 
 requestWakeLock();
+
+function customLog(message, type = 'log') {
+  var consoleDiv = document.getElementById('consoleDiv');
+  var newMessage = document.createElement('p');
+  
+  var styles = {
+    log: 'color: white;',
+    error: 'color: red;',
+    warn: 'color: orange;',
+    info: 'color: lightblue;'
+  };
+
+  newMessage.style = styles[type] || styles.log;
+
+  newMessage.textContent = message;
+  consoleDiv.appendChild(newMessage);
+  consoleDiv.scrollTop = consoleDiv.scrollHeight;
+}
+
+if (showConsole) {
+  const originalLog = console.log;
+  consoleDiv.style.display = 'block';
+  
+  console.log = function(message, ...args) {
+    customLog(message, 'log');
+    originalLog.apply(console, [message, ...args]);
+  };
+
+  console.error = function(message, ...args) {
+    customLog(message, 'error');
+    originalLog.apply(console, ['%c' + message, 'color: red;', ...args]);
+  };
+
+  console.warn = function(message, ...args) {
+    customLog(message, 'warn');
+    originalLog.apply(console, ['%c' + message, 'color: orange;', ...args]);
+  };
+
+  console.info = function(message, ...args) {
+    customLog(message, 'info');
+    originalLog.apply(console, ['%c' + message, 'color: blue;', ...args]);
+  };
+  
+  window.onerror = function(message, source, lineno, colno, error) {
+    customLog(`Uncaught error: ${message} at ${source}:${lineno}:${colno}`, 'error');
+    console.error(error);
+    return true;
+  };
+
+  window.onunhandledrejection = function(event) {
+    customLog(`Unhandled promise rejection: ${event.reason}`, 'error');
+    console.error(event.reason);
+  };
+
+  console.log('Console enabled');
+}
